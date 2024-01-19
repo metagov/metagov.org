13b372e9e0d10eb9b9a59e9ef8628da56e960cbd
Jack MB <jack@designandcode.com>
2024-01-19 12:39:17 +0100

    clean up person controller

--- site/controllers/people.php
+++ site/controllers/people.php
@@ -5,9 +5,6 @@ return function ($page, $pages, $site, $kirby) {
   $roles = $page->children()->pluck("role", ",", true);
   $researchInterests = $page->children()->pluck("interests", ",", true);
   $teamSize = 0;
-  $teamSplit = [[], [], []];
-  $index = 0;
-  $teamSplitCounter = 1;
   foreach ($page->team()->split() as $role) :   // get total size of team
     $people = $page->children()->filterBy('role', $role, ',');
     foreach ($people as $person) :

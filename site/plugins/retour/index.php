d31352ac6d75e43119adb34c604a3ad3d4e727b4
Jack MB <jack@designandcode.com>
2024-03-01 16:19:15 +0100

    add redirect plugin for testing

---
+++ site/plugins/retour/index.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Retour for Kirby Plugin
+ *
+ * Easily add and manage redirects from the
+ * Kirby 4 Panel and track 404 errors
+ *
+ * @package   Retour for Kirby
+ * @author    Nico Hoffmann <nico@getkirby.com>
+ * @link      https://github.com/distantnative/retour-for-kirby
+ * @copyright Nico Hoffmann
+ * @license   https://opensource.org/licenses/MIT
+ */
+
+use Kirby\Cms\App as Kirby;
+use Composer\Semver\Semver;
+
+// validate Kirby version
+if (Semver::satisfies(Kirby::version() ?? '0.0.0', '~4.0') === false) {
+    throw new Exception('Retour 5 requires Kirby 4');
+}
+
+// load classes
+require_once 'src/bootstrap.php';
+
+// register the plugin
+Kirby::plugin('distantnative/retour', [
+    'areas'        => require_once 'src/extensions/areas.php',
+    'hooks'        => require_once 'src/extensions/hooks.php',
+    'routes'       => require_once 'src/extensions/routes.php',
+    'translations' => require_once 'src/extensions/i18n.php'
+]);

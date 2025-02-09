aea5db1e47c09fa28d54ff39b2383a0809c77b3f
ddouglascarr <>
2025-02-10 10:47:32 +1100

    center and shrink images, responsive layout

--- site/snippets/pages/gallery.php
+++ site/snippets/pages/gallery.php
@@ -2,7 +2,7 @@
   <div class="mb-12">
   <h1 class="text-xl font-black text-brand mb-2">Deliberative Tool Gallery</h1>
   </div>
-  <div class="flex space-x-4">
+  <div class="flex flex-col md:flex-row space-x-4">
     <div class="flex-none w-64 divide-y">
       <div>
         <h4 class="font-black text-brand pb-2">Stage</h4>
@@ -31,19 +31,15 @@
 
     </div>
     <div class="flex-1">
-      <ul class="grid sm:grid-cols-2 xl:grid-cols-3 gap-4 sm:gap-6 mx-auto list">
+      <ul class="grid lg:grid-cols-2 xl:grid-cols-3 gap-4 sm:gap-6 list">
         <?php foreach ($page->children()->sortBy('title', 'asc') as $project) : ?>
-          <li class="list-none" data-title="<?= $project->title() ?>" data-status="<?= $project->projectStatus() ?? null ?>" data-type="<?= $project->type() ?? null ?>" data-category="<?= $project->category() ?? null ?>" data-stage="<?= $project->stage() ?? null ?>" data-participants="<?= ($project->seekingParticipants()->toBool()) ? 'Yes' : 'No' ?>">
+          <li class="list-none w-64 h-80" data-title="<?= $project->title() ?>" data-status="<?= $project->projectStatus() ?? null ?>" data-type="<?= $project->type() ?? null ?>" data-category="<?= $project->category() ?? null ?>" data-stage="<?= $project->stage() ?? null ?>" data-participants="<?= ($project->seekingParticipants()->toBool()) ? 'Yes' : 'No' ?>">
             <a href="<?= $project->url() ?>">
               <?php snippet('window', ['title' => $project->title(), 'subheading' => $project->subheading()], slots: true) ?>
               <?php if ($image = $project->cover()->toFile()) : ?>
-                <img class="w-full" src="<?= $image->crop(434, 235, "center")->url() ?>" srcset="<?= $image->srcset(
-                                                                                                    [
-                                                                                                      '1x'  => ['width' => 434, 'height' => 235, 'crop' => 'center'],
-                                                                                                      '2x'  => ['width' => 868, 'height' => 470, 'crop' => 'center'],
-                                                                                                      '3x'  => ['width' => 1320, 'height' => 705, 'crop' => 'center'],
-                                                                                                    ]
-                                                                                                  ) ?>" alt="<?= $image->alt()->esc() ?>" width="<?= $image->resize(434)->width() ?>" height="<?= $image->resize(235)->height() ?>">
+                <div class="flex grow items-center justify-items-center h-full p-2 overflow-hidden">
+                  <img class="object-contain" src="<?= $image->url() ?>"  alt="<?= $image->alt()->esc() ?>" />
+                </div>
               <?php endif ?>
               <?php endsnippet() ?>
             </a>

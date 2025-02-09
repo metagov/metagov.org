fc278ac3705aee473646c487ee383a18cce87b0e
ddouglascarr <>
2025-02-10 09:06:21 +1100

    remove dual link

--- site/snippets/pages/gallery.php
+++ site/snippets/pages/gallery.php
@@ -12,7 +12,6 @@
             [
               'filters' => [
                 'Framing',
-                'Elicition',
                 'Elicitation',
                 'Learning',
                 'Deliberation',

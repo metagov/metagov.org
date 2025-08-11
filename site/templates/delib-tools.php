4eaf25936dfaf7515d503a1f7bf2c4c5d6302700
Jenny Fan <fan.jenny@gmail.com>
2025-08-11 14:01:31 -0400

    Change delib tools slug

---
+++ site/templates/delib-tools.php
@@ -0,0 +1,5 @@
+<?php snippet('header') ?>
+
+<?php snippet('pages/delib-tools', ['page' => $page]) ?>
+
+<?php snippet('footer') ?>
--- site/templates/deliberative-tool-gallery.php
+++
@@ -1,5 +0,0 @@
-<?php snippet('header') ?>
-
-<?php snippet('pages/deliberative-tool-gallery', ['page' => $page]) ?>
-
-<?php snippet('footer') ?>
